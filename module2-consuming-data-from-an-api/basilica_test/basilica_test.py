58d825b5d08256dc33bf4e04a6e510902b59554d
Zhenya Warshavsky <zwarshavsky@gmail.com>
2019-11-14 12:40:01 -0500

    committing

---
+++ module2-consuming-data-from-an-api/basilica_test/basilica_test.py
@@ -0,0 +1,9 @@
+import basilica
+# sentences = [
+#     "This is a sentence!",
+#     "This is a similar sentence!",
+#     "I don't think this sentence is very similar at all...",
+# ]
+# with basilica.Connection('355af2f6-607d-6fde-77be-291908c5468f') as c:
+#     embeddings = list(c.embed_sentences(sentences))
+# print(embeddings)
