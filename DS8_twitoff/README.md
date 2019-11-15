2ddc96cd6f1114a3c7964d7f1c4d65215b1b1bff
Zhenya Warshavsky <zwarshavsky@gmail.com>
2019-11-15 11:15:43 -0500

    commit!

--- DS8_twitoff/README.md
+++
@@ -1,2 +0,0 @@
-# DS8_twitoff
-write a fun web application for comparing and predicting tweets
--- DS8_twitoff/TWITOFF/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Entry point for our twitoff flask app"""
-
-from .app import create_app
-
-#APP is global variable
-APP = create_app()
-
-#run this in terminal with FLASK_APP=TWITOFF:APP flask run
